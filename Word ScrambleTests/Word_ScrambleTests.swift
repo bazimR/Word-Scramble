80235b5a8505e783fb99bc5d1a8f630e823269f7
bazimR <rishaalmuhammed@gmail.com>
2025-02-18 23:47:11 +0530

    Initial Commit

---
+++ Word ScrambleTests/Word_ScrambleTests.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleTests.swift
+//  Word ScrambleTests
+//
+//  Created by Rishal Bazim on 18/02/25.
+//
+
+import Testing
+@testable import Word_Scramble
+
+struct Word_ScrambleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
